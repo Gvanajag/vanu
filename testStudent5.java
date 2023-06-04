8272b661f3057489c67d377261e4149bbe27bc63
Gvanajag <131242089+Gvanajag@users.noreply.github.com>
2023-06-04 16:02:06 +0530

    Add files via upload

---
+++ testStudent5.java
@@ -0,0 +1,22 @@
+class Student{
+int rollno;
+String name;
+Student(){rollno=0;name=null;}
+Student(int r,String n){
+rollno=r;
+name=n;
+}
+void displayinformation(){System.out.println(rollno+""+name);}
+}
+class testStudent5{
+public static void main(String[]args){
+Student s1;
+s1=new Student();
+Student s2=new Student(587,"vini");
+s1.displayinformation();
+s2.displayinformation();
+
+
+    } 
+   }
+ 

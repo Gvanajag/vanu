489d43dde5601d64ee3c783420d6a7f02cd6c70f
Gvanajag <131242089+Gvanajag@users.noreply.github.com>
2023-06-05 17:53:40 +0530

    Add files via upload

---
+++ if_else_example1.java
@@ -0,0 +1,19 @@
+ import java.util.*;
+class if_else_example1{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i=sc.nextInt();
+int j=sc.nextInt();
+if(j==0)
+{
+System.out.println("Division by errpr");
+}
+
+else
+ System.out.println(i+"divided by"+j+"is"+(i/j));
+i=i+j;
+
+
+}
+}
